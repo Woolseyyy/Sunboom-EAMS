5c6e19308146d684585bf2e2b5b58568a48fe0e4
LiuYang <651636074@qq.com>
2017-06-20 16:36:13 +0800

    repair course submit latest homework

--- Page/Student/Component/CourseInfo/CourseInfo.jsx
+++ Page/Student/Component/CourseInfo/CourseInfo.jsx
@@ -49,12 +49,12 @@ class Entry extends React.Component
     }
 
     submitLatestHomework = () => {
-        this.refs.uploadHomeworBrowser.click();
+        this.refs.uploadLatestHomeworkBrowser.click();
     }
 
     submitHomework = (e) => {
         this.setState({homeworkID: e});
-        this.refs.uploadLatestHomeworBrowser.click();
+        this.refs.uploadHomeworkBrowser.click();
     }
 
     handleGradeReview = () => {
@@ -261,8 +261,8 @@ class Entry extends React.Component
                         {this.state.dialogMsg}
                     </Dialog>
                 </div>
-                <input type="file" id="file" ref="uploadHomeworBrowser" style={{display: "none"}} onChange={(files, homeworkID) => this.props.handleSubmitHomework(this.refs.uploadHomeworBrowser.files, this.state.homeworkID)}/>
-                <input type="file" id="file" ref="uploadLatestHomeworBrowser" style={{display: "none"}} onChange={(files) => this.props.handleSubmitLatestHomework(this.refs.uploadLatestHomeworBrowser.files)}/>
+                <input type="file" id="file" ref="uploadHomeworkBrowser" style={{display: "none"}} onChange={(files, homeworkID) => this.props.handleSubmitHomework(this.refs.uploadHomeworkBrowser.files, this.state.homeworkID)}/>
+                <input type="file" id="file" ref="uploadLatestHomeworkBrowser" style={{display: "none"}} onChange={(files) => this.props.handleSubmitLatestHomework(this.refs.uploadLatestHomeworkBrowser.files)}/>
             </div>
         );
     }
