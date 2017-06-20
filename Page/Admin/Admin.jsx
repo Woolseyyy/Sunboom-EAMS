40df28ba33c696fe902913893c8386498148db10
woolseyyy <woolsey@zju.edu.cn>
2017-06-20 12:00:11 +0800

    complete admin- course filter function

--- Page/Admin/Admin.jsx
+++ Page/Admin/Admin.jsx
@@ -1,6 +1,9 @@
-var React=require("react");
-var style=require("./Admin.css");
+import React, {Component} from "react";
+import css from "./Admin.css";
 //var Helmet=require("react-helmet");
+
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 import ApplyList from "./Component/ApplyList/ApplyList.jsx";
 import Detail from "./Component/Detail/Detail.jsx";
@@ -10,6 +13,13 @@ import Header from "../../Common/Header/Header.jsx"
 
 //material-ui
 import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import Toggle from 'material-ui/Toggle';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Entry extends React.Component{
     constructor(props) {
@@ -160,7 +170,8 @@ class Entry extends React.Component{
             {"text": "教学资源申请", icon:"BookMark"}
         ];
         var afterDeviderItems = [
-            {"text": "已受理申请", icon:"Done"}
+            {"text": "已受理申请", icon: "Done"},
+            {"text": "选课管理", icon: "BookMark"}
         ];
         return(
             <div className="background header-bottom-container">
@@ -190,12 +201,200 @@ class Entry extends React.Component{
                             }
                         }.bind(this))
                     }
+                    {(this.state.selectID == 6) ? <Assignment/> : null}
                 </Paper>
                 <Detail open={this.state.detailOpen}
                         title={this.state.list[this.state.listID].title}
                         data={this.state.list[this.state.listID].data[this.state.itemID]}
                         HandleClose={this.HandleClose}
                 />
+            </div>
+        )
+    }
+}
+
+class Assignment extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modify: false,
+            modifyBatch: {},
+            startFilterCB: {},
+            openAlert: false,
+            assignment: [
+                {
+                    id: 1,
+                    teacher: '李玺',
+                    name: '人工智能',
+                    volume: '30'
+                },
+                {
+                    id: 2,
+                    teacher: '许威威',
+                    name: '三维重建',
+                    volume: '20'
+                },
+            ]
+        }
+    }
+
+    componentDidMount() {
+        //this.getAssignmentInfo();
+    }
+
+    getAssignmentInfo() {
+        fetch(localStorage.root_url + 'api/Assignment/AllAssignment',
+            {
+                method: "GET",
+                headers: {
+                    "Authorization": localStorage.token
+                }
+            }
+        )
+            .then((response) => response.json())
+            .then((cb) => {
+                //console.log(cb.data);
+                //console.log(window.eams.obParse(cb.data));
+                let data = window.eams.obParse(cb.data);
+
+                this.setState({assignment: data})
+            });
+    }
+
+    handleOpen = () => {
+        this.setState({openAlert: true});
+    };
+
+    handleClose = () => {
+        this.setState({openAlert: false});
+    };
+
+    render() {
+        return (
+            <div >
+                <div style={{position: "relative"}}>
+                    <div className={css.cardTitle + " " + css.inline}>教学班容量</div>
+                    <FlatButton label="开始筛选" style={{margin: '0 20px'}} primary={true} onTouchTap={() => {
+                        fetch(localStorage.root_url + 'api/Enrollment/StartFilter',
+                            {
+                                method: 'POST',
+                                headers: {
+                                    "Authorization": localStorage.token
+                                }
+                            })
+                            .then((response) => response.json())
+                            .then((cb) => {
+                                //console.log(cb.data);
+                                //console.log(window.eams.obParse(cb.data));
+                                switch (cb.errorCode) {
+                                    case 200: {
+                                        this.setState({
+                                            startFilterCB: {
+                                                state: 'success',
+                                                text: '筛选请求发送成功，服务器已开始筛选'
+                                            }
+                                        });
+                                        break;
+                                    }
+                                    case 400: {
+                                        this.setState({
+                                            startFilterCB: {
+                                                state: 'error',
+                                                text: '服务器正在筛选，请不要重复发送筛选请求'
+                                            }
+                                        });
+                                        break;
+                                    }
+                                    default: {
+                                        this.setState({
+                                            startFilterCB: {
+                                                state: 'error',
+                                                text: '请求发送失败，请稍后重试'
+                                            }
+                                        });
+                                    }
+                                }
+                                this.handleOpen();
+                            });
+                    }}/>
+                    <Dialog
+                        title={"请求发送" + ((this.state.startFilterCB.state === 'success') ? "成功" : "失败")}
+                        actions={
+                            <FlatButton
+                                label="好的"
+                                primary={true}
+                                onTouchTap={this.handleClose}
+                                keyboardFocused={true}
+                            />
+                        }
+                        modal={false}
+                        open={this.state.openAlert}
+                        onRequestClose={this.handleClose}
+                    >
+                        {this.state.startFilterCB.text}
+                    </Dialog>
+                    <div className={css.cardTitleButton + " " + css.inline}>
+                        <Toggle
+                            label="修改容量"
+                            defaultToggled={this.state.modify}
+                            onTouchTap={() => {
+                                //submit
+                                let modifyBatch = this.state.modifyBatch;
+                                for (let key in modifyBatch) {
+                                    var formData = new FormData();
+                                    formData.append("id", key);
+                                    formData.append("volume", modifyBatch[key]);
+
+                                    fetch(localStorage.root_url + 'api/Enrollment/SetEnrollable',
+                                        {
+                                            method: 'POST',
+                                            headers: {
+                                                "Authorization": localStorage.token
+                                            },
+                                            body: formData
+                                        });
+                                }
+
+                                let modify = this.state.modify;
+                                this.setState({modify: !modify, modifyBatch: {}})
+                            }
+                            }
+                        />
+                    </div>
+                </div>
+                <Divider/>
+                <div className={css.listTableContainer}>
+                    <Table selectable={false}>
+                        <TableBody displayRowCheckbox={false}>
+                            {
+                                this.state.assignment.map((item, index) => {
+                                    return (
+                                        <TableRow key={index} displayBorder={false}>
+                                            <TableRowColumn style={{width: '80px'}}>{item.id}</TableRowColumn>
+                                            <TableRowColumn style={{width: '80px'}}>{item.name}</TableRowColumn>
+                                            <TableRowColumn style={{width: '60px'}}>{item.teacher}</TableRowColumn>
+                                            <TableRowColumn/>
+                                            <TableRowColumn style={{width: '50px'}}>
+                                                <TextField
+                                                    fullWidth={true}
+                                                    inputStyle={{textAlign: "center"}}
+                                                    name={"score" + index}
+                                                    disabled={!this.state.modify}
+                                                    defaultValue={item.volume}
+                                                    onChange={(event, value) => {
+                                                        let modifyBatch = this.state.modifyBatch;
+                                                        modifyBatch[item.id] = value;
+                                                        this.setState({modifyBatch: modifyBatch});
+                                                    }}
+                                                />
+                                            </TableRowColumn>
+                                        </TableRow>
+                                    )
+                                })
+                            }
+                        </TableBody>
+                    </Table>
+                </div>
             </div>
         )
     }
